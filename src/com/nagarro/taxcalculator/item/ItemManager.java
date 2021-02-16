a14485d30db5e89ded32df1d0480473e865ac40f
Arpit Garg <arpit.garg02@nagarro.com>
2021-02-16 17:06:07 +0530

    Java Doc added

--- src/com/nagarro/taxcalculator/item/ItemManager.java
+++ src/com/nagarro/taxcalculator/item/ItemManager.java
@@ -1,3 +1,20 @@
+/*
+* Class name: ItemManager
+*
+* Version info: jdk 1.8
+*
+* Copyright notice:
+* 
+* Author info: Arpit Garg
+*
+* Creation date: 16/Feb/2021
+*
+* Last updated By: Arpit Garg
+*
+* Last updated Date: 16/Feb/2021
+*
+* Description: Used to take user inputs and handles all the excpetion 
+*/
 package com.nagarro.taxcalculator.item;
 
 import java.util.ArrayList;
@@ -58,13 +75,16 @@ public class ItemManager {
         displayItems(itemsInfo);
     }
 
-    private void displayItems(ArrayList<Item> itemsInfo) {
+    /**
+     * Display the details of the item
+     */
+    private void displayItems(ArrayList<Item> itemInfo) {
         System.out.println("--------------------------------------------------------------------------------------");
         System.out.printf("%10s %10s %15s %15s %15s %15s", "Name", "Price", "Quantity", "Type", "Sales Tax",
                 "Final Price");
         System.out.println();
         System.out.println("--------------------------------------------------------------------------------------");
-        for (Item item : itemsInfo) {
+        for (Item item : itemInfo) {
             System.out.printf("%10s %10s %15s %15s %15s %15s", item.getItemName(), item.getItemPrice(),
                     item.getItemQuantity(), item.getItemType(), item.getTaxPrice(), item.getTotalTax());
             System.out.println();
